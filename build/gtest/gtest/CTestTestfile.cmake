c38418f17f5cbc5ae537f7d007ea627dd61084dc
Deepak Gopinath <musicmagic@gmail.com>
2019-05-03 16:06:13 -0500

    Removed build devel

--- build/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/Code/point_robot_2d_processing/build/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ros_processing_bridge/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "rospy".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "ros_processing_bridge"
-PROJECT_SPACE_DIR = "/home/Code/point_robot_2d_processing/install"
-PROJECT_VERSION = "0.0.0"
